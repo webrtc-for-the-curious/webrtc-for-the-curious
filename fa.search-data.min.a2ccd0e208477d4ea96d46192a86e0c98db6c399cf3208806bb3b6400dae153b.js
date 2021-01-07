2e29e7c48662bd596d0ce78a2604a3ce2a39c7f0
Sean-Der <Sean-Der@users.noreply.github.com>
2021-01-07 18:09:42 +0000

    deploy: 0b50182cca7e80c838286aae73e05e08f9cfa0a6

---
+++ fa.search-data.min.a2ccd0e208477d4ea96d46192a86e0c98db6c399cf3208806bb3b6400dae153b.js
@@ -0,0 +1,1 @@
+'use strict';(function(){const indexCfg={cache:true};indexCfg.doc={id:'id',field:['title','content'],store:['title','href','section'],};const index=FlexSearch.create('balance',indexCfg);window.bookSearchIndex=index;})();
